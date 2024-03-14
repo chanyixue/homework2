505e33986ff98b33ec2ac8df548e3077f1261078
S12240017 <161661096+chanyixue@users.noreply.github.com>
2024-03-14 08:30:04 +0800

    Add files via upload

---
+++ 偶數.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 08:25:34 2024
+
+@author: Student
+"""
+
+num = int(input("請輸入一個整數："))
+
+if num % 2 == 0:
+    print(f"{num} 是偶數。")
+else:
+    print(f"{num} 不是偶數。")
